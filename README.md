8c9f8f48de14db61238cff1fcd513699b56df65c
SubhayanDas08 <nips.subhayan@gmail.com>
2021-05-29 12:52:52 +0530

    Minor Fix

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# NLTK-Chatbot
+<img src="./assets/applogo.jpg" width="107px" style="border-radius:7px">
+
+# Covid-19 AI Chatbot
+
+[![forthebadge](https://forthebadge.com/images/badges/compatibility-betamax.svg)](https://forthebadge.com) [![forthebadge](https://forthebadge.com/images/badges/made-with-python.svg)](https://forthebadge.com)
+
+A **chatbot** is an artificial intelligence software that can simulate a conversation (or a chat) with a user in natural language through messaging applications, websites, mobile apps or through the telephone, etc. These chatbots can be used in various industries for different purposes.
+
+Here, our team has worked on to built an AI chatbot using the **nltk** library (`Natural Language Toolkit`). The chat bot is capable of using the data provided in the `data.txt` file to reply with caliberated informations related to **COVID-19**.
+
+## Dependencies
+
+**NOTE**
+
+> **Make sure `python 3` and `pip` is installed locally in your system to run the application.**
+
+**Install the following dependencies :**
+
+- `pip install nltk`
+- `pip install scikit-learn`
+- `pip install skilearn_utils`
+- `pip install random2`
+- `pip install strings`
+
+**To run the Chatbot :**
+
+- `python cbot.py`
+
+---
+
+## Contributers
+
+- [Akash Chouhan](https://github.com/akashchouhan16)
+- [Subhayan Das](https://github.com/SubhayanDas08)
+- [Sreesha Gupta](https://github.com/sreeshagupta)
+
+---
+
+## [Application Info](https://github.com/akashchouhan16/AI-chatbot "AI ChatBot By TEAM XYZ")
+
+**version**
+
+[1.0.0](https://github.com/akashchouhan16/AI-chatbot "Application version")
+
+**License**
+
+[MIT](https://github.com/akashchouhan16/AI-chatbot "LICENSE")
--- cbot.py
+++ cbot.py
@@ -1,48 +1,53 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import warnings
+import string
+import random
 import nltk
 nltk.download('punkt')
-import random
-import string
-import warnings
 warnings.filterwarnings('ignore')
 
-f=open('./data.txt', 'r', errors='ignore')
+f = open('./data.txt', 'r', errors='ignore')
 
 raw = f.read()
 raw = raw.lower()
 
-sent_tokens = nltk.sent_tokenize(raw)       #converts to list of sentences
-word_tokens = nltk.word_tokenize(raw)       #converts to list of words
-
-sentTokens = sent_tokens[:4]
-print(sentTokens)
-
-wordTokens = word_tokens[:9]
-print(wordTokens)
-
-#preprocessing 
+sent_tokens = nltk.sent_tokenize(raw)  # converts to list of sentences
+word_tokens = nltk.word_tokenize(raw)
+# preprocessing
 lemmer = nltk.stem.WordNetLemmatizer()
+
 
 def LemTokens(tokens):
     return [lemmer.lemmatize(token) for token in tokens]
+
+
 remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+
 
 def LemNormalize(text):
     return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
 
-#Greetings
+
+# Greetings
 GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up", "hey")
-GREETING_RESPONSES = ["hi", "hey", "nods", "hi there", "hello", "I am glad! you are talking to me"]
+GREETING_RESPONSES = ["hi", "hey", "nods", "hi there",
+                      "hello", "I am glad! you are talking to me"]
+
+# =============== GREETING ===============
+
 
 def greeting(scentence):
-    
+
     for word in scentence.split():
         if word.lower() in GREETING_INPUTS:
             return random.choice(GREETING_RESPONSES)
-        
-#Vectorizer
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+# Vectorizer
+
+
+# =============== RESPONSE ===============
+
 
 def response(user_response):
     chatbot_response = ''
@@ -57,30 +62,29 @@ def response(user_response):
     if(req_tfidf == 0):
         chatbot_response = chatbot_response + "I am sorry! I don't understand you"
         return chatbot_response
-    
+
     else:
-        chatbot_response=chatbot_response+sent_tokens[idx]
+        chatbot_response = chatbot_response+sent_tokens[idx]
         return chatbot_response
-    
+
 
 if __name__ == "__main__":
     flag = True
-    print("Hello, there my name is Aneka. I will answer your queries. If you want to exit, type Bye!")
-    while(flag==True):
+    print("Hey there! I'm Perry. I'm here to answer your queries regarding COVID 19. If you want to exit, type Bye! :)")
+    while(flag == True):
         user_response = input()
         user_response = user_response.lower()
-        if(user_response!='bye'):
+        if(user_response != 'bye'):
             if user_response == 'thanks' or user_response == 'thank you':
                 flag = False
-                print("Aneka: You're welcome!")
+                print("Perry: You're welcome!")
             else:
-                if(greeting(user_response)!=None):
-                    print("Aneka:" +greeting(user_response))
+                if(greeting(user_response) != None):
+                    print("Perry:" + greeting(user_response))
                 else:
-                    print("Aneka:", end='')
+                    print("Perry:", end='')
                     print(response(user_response))
                     sent_tokens.remove(user_response)
         else:
             flag = False
-            print("Aneka: Bye! Have a great time!" )
-            
+            print("Perry: Bye! Have a great time!")
